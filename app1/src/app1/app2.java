3b01a42d516d8953dbcbfdc687e7ebb3b8702667
ELCOT <ELCOT@192.168.1.47>
2023-07-22 11:40:54 -0700

    I am create a login

---
+++ app1/src/app1/app2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1;
+
+import javax.swing.JFrame;
+
+
+public class app2 {
+    public void run(){
+        JFrame f=new JFrame("Welcome");
+        f.setVisible(true);
+        f.setBounds(100, 100, 1000, 700);
+        f.setLayout(null);
+    }
+}
