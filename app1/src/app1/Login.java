6e012cce00dbeb3fa1951d8c4771727317af8b5c
SuviSiva <rsuvisivadme2000@gmail.com>
2023-07-22 11:41:32 +0530

    i am start login page

---
+++ app1/src/app1/Login.java
@@ -0,0 +1,12 @@
+
+package app1;
+import javax.swing.*;
+public class Login {
+ JFrame f = new JFrame();
+    public void run()
+    {
+        f.setVisible(true);
+        f.setLayout(null);
+        f.setSize(600,400);
+    }
+}
